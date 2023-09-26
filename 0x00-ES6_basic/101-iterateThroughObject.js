3300f3ffc5ed540870bfa54751fec073b91412f2
seb-art <kimanzikims300@gmail.com>
2023-09-26 08:11:25 +0000

    task 12 fix

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const employees = [];
-  for (let employee of reportWithIterator) {
-    employees.push(employee);
-  }
-  return employees.join(' | ');
+	const employees = [];
+	
+	for (let employee of reportWithIterator) {
+		employees.push(employee);
+	}
+	
+	return employees.join(' | ');
 }

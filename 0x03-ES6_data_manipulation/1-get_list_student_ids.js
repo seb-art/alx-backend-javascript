e3e9665dfb3a83ab5f6fed1cca37b81dfcfe487c
seb-art <kimanzikims300@gmail.com>
2023-10-03 07:35:31 +0000

    task 0-5

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,16 @@
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Sebastian John  <https://github.com/seb-art>
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
+}

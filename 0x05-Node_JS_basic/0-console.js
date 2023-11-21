cd3d8d1cc1314d4a3ed2c4651b8cade0ca0f6116
seb-art <kimanzikims300@gmail.com>
2023-11-21 20:54:20 +0000

    task 1

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Sebastian Kimanzi <https://github.com/seb-art>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
